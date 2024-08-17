ccc5d8ec2f09ec450a0a67a18c804c91e68aa4b2
Alister <alisterfernandes1210@gmail.com>
2024-08-17 10:03:45 +0530

    4:10 AppointmentModal component start

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,8 +3,12 @@ import Link from "next/link";
 import React from "react";
 import { Label } from "@radix-ui/react-label";
 import StatCard from "@/components/StatCard";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
+import { DataTable } from "@/components/table/DataTable";
+import { columns, Payment } from "@/components/table/columns";
 
-const Admin = () => {
+const Admin = async () => {
+  const appointments = await getRecentAppointmentList();
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -31,23 +35,24 @@ const Admin = () => {
         <section className="admin-stat">
           <StatCard
             type="appointments"
-            count={5}
+            count={appointments.scheduledCount}
             label="Scheduled appointments"
             icon="/assets/icons/appointments.svg"
           />
           <StatCard
             type="pending"
-            count={10}
+            count={appointments.pendingCount}
             label="Pending appointments"
             icon="/assets/icons/pending.svg"
           />
           <StatCard
             type="cancelled"
-            count={2}
+            count={appointments.cancelledCount}
             label="Cancelled appointments"
             icon="/assets/icons/cancelled.svg"
           />
         </section>
+        <DataTable columns={columns} data={appointments.documents} />
       </main>
     </div>
   );
--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import React from "react";
+import Image from "next/image";
 
 interface StatCardProps {
   type: "appointments" | "pending" | "cancelled";
@@ -17,7 +18,17 @@ const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
         "bg-cancelled": type === "cancelled",
       })}
     >
-      StatCard
+      <div className="flex items-center gap-4">
+        <Image
+          src={icon}
+          height={32}
+          width={32}
+          alt={label}
+          className="size-8 w-fit"
+        />
+        <h2 className="text-32-bold text-white">{count}</h2>
+      </div>
+      <p className="text-14-regular">{label}</p>
     </div>
   );
 };
