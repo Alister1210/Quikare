1936f53c0030b81fc9f9cdcdf7399c41b575400e
Alister <alisterfernandes1210@gmail.com>
2024-08-24 20:48:59 +0530

    logo remaining

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,7 +3,7 @@ import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from "@sentry/nextjs";
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
   Sentry.metrics.set("user_view_register", user.name);
@@ -12,18 +12,16 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
       <section className="remove-scrollbar container ">
         <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/qk-logo.svg"
             height={1000}
             width={1000}
             alt="patient"
-            className="mb-12 h-10 w-fit"
+            className="mb-2 h-24 w-fit"
           />
 
           <RegisterForm user={user} />
 
-          <p className="copyright py-12">
-            © 2024 Quikare
-          </p>
+          <p className="copyright py-12">© 2024 Quikare</p>
         </div>
       </section>
       <Image
